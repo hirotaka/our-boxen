b3882246c00dcc90f284f87c6af1d38462c2da81
水谷裕生 <hirotaka@mizutaniyuuki-no-MacBook-Pro.local>
2015-02-07 00:35:12 +0900

    Update our-boxen

---
+++ modules/people/manifests/hirotaka.pp
@@ -0,0 +1,38 @@
+# manifests/people/manifests/sonots.pp
+class people::hirotaka {
+  include zsh
+  include chrome
+  include iterm2::stable
+  include virtualbox
+  include dropbox
+  include karabiner
+  include vagrant
+  include emacs
+  include evernote
+  include skype
+  include adobe_reader
+  include postgresql
+  include heroku
+  include onepassword
+
+  # homebrew でインストールするもの
+  package {
+    [
+      'tmux',
+      'rbenv-gemset'
+    ]:
+  }
+
+  $home     = "/Users/${::boxen_user}"
+  $my       = "${home}/Documents/workspace"
+  $dotfiles = "${my}/dotfiles"
+
+  file { $my:
+    ensure  => directory
+  }
+
+  repository { $dotfiles:
+    source  => 'hirotaka/dotfiles',
+    require => File[$my]
+  }
+}
